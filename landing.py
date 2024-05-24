15decb87798983e9909fcb7e2133237160a22c5b
Allan <kiruiallan003@gmail.com>
2024-05-24 02:50:43 -0400

    PORTFOLIO

--- landing.py
+++ landing.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response,request, flash, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'not-advisable-string-for-prod'
+
+@app.route('/', methods=['GET', 'POST'])
 def landing():
+    if request.method == 'POST':
+        flash("This is still work under progress.")
+        return redirect('/')
     return make_response(render_template('index.html'))
 
 if __name__ == '__main__':
